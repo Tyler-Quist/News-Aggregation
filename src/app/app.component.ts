ff36ea1edc2604d07f2c64d75ce7fc7c50d944a4
Tyler Quist <tquist@worcester.edu>
2019-12-12 10:17:57 -0500

    added side list of sources with buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NewsService } from './news.service';
-import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {HttpClient, HttpClientModule, HttpErrorResponse} from '@angular/common/http';
+import {error} from 'util';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,7 @@ export class AppComponent {
 articles: Array<any>;
 sources: Array<any>;
 apiKey = '79f71650c65b4915adce89ce103cba40';
+ error: string = null;
   constructor(private newsapi: NewsService,private http: HttpClient) {
     console.log('app component working');
   }
@@ -19,11 +21,14 @@ apiKey = '79f71650c65b4915adce89ce103cba40';
     this.newsapi.getArticles().subscribe(data => this.articles = data['articles']);
     this.newsapi.getSources().subscribe(data=> this.sources = data['sources']);
   }
-  searchArticles(source){
+  searchArticles(source: String){
     console.log("selected source is: "+  source);
     this.newsapi.getArticlesById(source).subscribe(data => this.articles = data['articles']);
   }
   reset() {
     this.newsapi.getArticles().subscribe(data => this.articles = data['articles']);
+  }
+  getSources(){
+
   }
 }
